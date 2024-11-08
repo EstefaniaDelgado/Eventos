eca3f30e187eb6c4e1ec0657ee7e1c1fa54e0fbf
EstefaniaDelgado <teff_64@hotmail.com>
2024-11-07 22:44:08 -0500

    style(about): add margins in the image

--- src/views/Home/components/AboutEvent.jsx
+++ src/views/Home/components/AboutEvent.jsx
@@ -1,9 +1,9 @@
 const AboutEvent = () => {
   return (
     <section className="relative z-10 my-16 mx-5 max-w-[1200px] md:flex md:items-center md:gap-5 xl:mx-auto">
-      <div className="pt-6 flex flex-col justify-end items-start gap-5 md:flex-1 md:order-1 md:pl-5 xl:p-5 ">
+      <div className="pt-5 md:pt-8 lg:pt-16 xl:pt-24 flex flex-col justify-end items-start gap-5 md:flex-1 md:order-1 md:pl-5 xl:p-5 ">
         {' '}
-        <h2 className="absolute top-0 text-5xl font-extrabold text-gray-900 md:text-6xl lg:text-8xl">ABOUT</h2>
+        <h2 className="absolute top-0 text-5xl font-extrabold text-gray-900 md:text-6xl lg:text-8xl xl:text-9xl">ABOUT</h2>
        <div className="z-50">
        <h3 className="font-light z-50 text-xl font-title text-purple-600">
           [ Sobre el <strong>Evento ]</strong>
@@ -15,9 +15,9 @@ const AboutEvent = () => {
       </div>
 
       <div className=" flex flex-col gap-2 mt-5 md:w-full md:mt-0 md:flex-1 md:pl-5">
-       <div className=" h-20 md:h-24 w-4/5 bg-artist bg-top bg-cover rounded-lg "></div>
-       <div className=" h-20 md:h-24  w-3/4 rounded-lg self-end  bg-artist bg-center bg-cover"></div>
-       <div className=" h-20 md:h-24 rounded-lg w-4/5 bg-artist bg-bottom bg-cover"></div>
+       <div className=" h-16 md:h-24 w-4/5  md:ml-8 lg:ml-16 lg:w-4/6 bg-artist bg-top bg-cover rounded-lg "></div>
+       <div className=" h-16 md:h-24 lg:mr-14  w-4/6 rounded-lg self-end  bg-artist bg-center bg-cover"></div>
+       <div className=" h-16 md:h-24 rounded-lg w-4/5 ml-5 md:ml-8 lg:ml-16 lg:w-4/6 bg-artist bg-bottom bg-cover"></div>
       </div>
   
      
