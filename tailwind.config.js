7b1fe82f5ff293e2760440019f1d02c2e2d7e7ff
EstefaniaDelgado <teff_64@hotmail.com>
2025-06-23 17:30:28 -0500

    refactor(external resources)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  important: "#root",
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  important: '#root',
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        subtitlePrimary: "#be185d", // pink
-        subtitleSecondary: "#9333ea", // purple
-        subtitleMuted: "#1c5d54", // greenJungle
-        itemsMenu:"rgb(193 133 3)" //yellow
+        subtitlePrimary: '#be185d', // pink
+        subtitleSecondary: '#9333ea', // purple
+        subtitleMuted: '#1c5d54', // greenJungle
+        itemsMenu: 'rgb(193 133 3)', //yellow
       },
       backgroundImage: {
-        "hero-image":
+        'hero-image':
           "linear-gradient(to bottom, rgba(0, 0, 0, 0) 50%, rgba(0, 0, 0, 0.9) 100%),url('../src/assets/hero.webp')",
         artist: "url('../src/assets/artist.jpg')",
         counter: "url('../src/assets/background-counter-date.webp')",
@@ -19,25 +19,27 @@ export default {
         party: "url('../src/assets/party_background.jpg')",
       },
       fontFamily: {
-        header: ["Rock Salt", "cursive"],
-        title: ["Space Mono", "monospace"],
+        header: ['Rock Salt', 'cursive'],
+        title: ['"Space Mono"', 'monospace'],
       },
     },
   },
-  plugins: [function ({ addBase }) {
-    addBase({
-      ".no-spin::-webkit-outer-spin-button": {
-        "-webkit-appearance": "none",
-        margin: "0",
-      },
-      ".no-spin::-webkit-inner-spin-button": {
-        "-webkit-appearance": "none",
-        margin: "0",
-      },
-      ".no-spin": {
-        "-moz-appearance": "textfield",
-      },
-    });
-  },
-  require("tailwind-scrollbar"),],
+  plugins: [
+    function ({ addBase }) {
+      addBase({
+        '.no-spin::-webkit-outer-spin-button': {
+          '-webkit-appearance': 'none',
+          margin: '0',
+        },
+        '.no-spin::-webkit-inner-spin-button': {
+          '-webkit-appearance': 'none',
+          margin: '0',
+        },
+        '.no-spin': {
+          '-moz-appearance': 'textfield',
+        },
+      });
+    },
+    require('tailwind-scrollbar'),
+  ],
 };
